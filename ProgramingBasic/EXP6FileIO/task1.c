c331a80a9d4f44381076403d1d5517df5355acd0
MysticalGuest <1317148109@qq.com>
2020-05-20 10:00:26 +0800

    Update task1.c

--- ProgramingBasic/EXP6FileIO/task1.c
+++ ProgramingBasic/EXP6FileIO/task1.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
+#include<Windows.h>
+
 struct student{
 	long num;
 	char name[20];
 	float score[3];
 }stu[5];
+
 void main(){
 	FILE *fp;
 	int i,j;
 	float sum;
-	printf("分别输入这5名学生的学号、姓名、3门课程成绩。\n");
+	// 分别输入这5名学生的学号、姓名、3门课程成绩
+	printf("Enter the student numbers, names, and grades of the three courses for the five students: \n");
 	for(i=0;i<5;i++)
 		scanf("%ld%s%f%f%f",&stu[i].num,stu[i].name,&stu[i].score[0],&stu[i].score[1],&stu[i].score[2]);
 		fp=fopen("stud.txt","w+");
@@ -19,7 +23,9 @@ void main(){
 		for(j=0;j<5;j++){
 			sum+=stu[j].score[i];
 		}
-		fprintf(fp,"第%d门课程成绩平均分是：%.2f\n",i+1,sum/5);
+		fprintf(fp,"%.2f\n",i+1,sum/5);
 	}
+
+	system("pause");
 
 }
