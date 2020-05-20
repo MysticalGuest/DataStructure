69f7372bf10520442c85f784de8b5e9abd8d111f
MysticalGuest <1317148109@qq.com>
2020-05-20 10:00:43 +0800

    Create birthday27th.c

---
+++ ProgramingBasic/Other/birthday27th.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include<Windows.h>
+
+void main(void){
+	int i;
+	printf("\n\n\n");
+	printf("%*.*s\n",48,21,"_*=_           _=*_");
+	printf("%*.*s\n",51,25,"XiaoMengli      Birthday!");
+	printf("%*.*s\n",53,29,"I wish you a happy birthday !");
+	printf("%*.*s\n",55,33,"Grandpa wishes you happy birthday!");
+	printf("%*.*s\n",56,35,"Grandma wishes you chuffed birthday!");
+	printf("%*.*s\n",56,35,"Dada wishes you a beatific birthday!");
+	printf("%*.*s\n",55,33,"Mommy wishes you a happy birthday!");
+	printf("%*.*s\n",54,31,"Mr Ma wishes you happy birthday!");
+	printf("%*.*s\n",53,29,"Families and Ladybros and All");
+	printf("%*.*s\n",51,25,"Wish happy 27th birthday!");
+	printf("%*.*s\n",49,21,"Bless my elder sister");
+	printf("%*.*s\n",47,17,"Healthy Fortunate!");
+	printf("%*.*s\n",45,13,"VeryBeautiful");
+	printf("%*.*s\n",43,9,"Endearing");
+	printf("%*.*s\n",41,5,"Honey");
+	printf("%*.*s\n",40,3,"XML");
+	printf("%*.*s\n",39,1,"Y");
+	for(i=0;i<5;i++)
+		printf("\n");
+
+	system("pause");
+}
