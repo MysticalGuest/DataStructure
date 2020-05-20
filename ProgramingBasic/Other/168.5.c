ac21a10ae3ba0490c293963f0af0f33a9e957bf4
MysticalGuest <1317148109@qq.com>
2020-05-20 10:56:32 +0800

    Update 168.5.c

--- ProgramingBasic/Other/168.5.c
+++ ProgramingBasic/Other/168.5.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 #include<Windows.h>
 
-void main()
-{
+void main(){
 	int i,j,a[5],max,temp;
+	printf("Please enter 5 integers: \n");
 	for(i=0;i<5;i++)
 		scanf("%d",&a[i]);
 	for(i=5;i>0;i--)
 		for(j=0;j<i-1;j++)
-			if(a[j+1]<a[j])
-		{
-			temp=a[j];
-			a[j]=a[j+1];
-			a[j+1]=temp;
-		}
+			if(a[j+1]<a[j]){
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		printf("After sorting: ");
     for(i=0;i<5;i++)
-		printf("%5d",a[i]);
-	printf("\n");
+			printf("%5d",a[i]);
+		printf("\n");
 
 	system("pause");
 }
