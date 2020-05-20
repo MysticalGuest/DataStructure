b99fce50736dca4433097cda5672f7d2d3156fe0
MysticalGuest <1317148109@qq.com>
2020-05-20 10:00:18 +0800

    Update task2.c

--- ProgramingBasic/EXP5Structure/task2.c
+++ ProgramingBasic/EXP5Structure/task2.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<Windows.h>
 
 #define LEN sizeof(struct student)
 
@@ -15,18 +16,23 @@ struct student *creat(void){
 	struct student *head;
 	struct student *p1,*p2;
 	p1=p2=(struct student *)malloc(LEN);
-	printf("分别输入每个学生的学号、性别、年龄、姓名。(以输入0结束输入)\n");
+	// 分别输入每个学生的学号、性别、年龄、姓名。(以输入0结束输入)
+	printf("Enter the student ID, gender, age, and name of each student separately. (End input with input 0) \n");
 	scanf("%ld,%c,%d,%s", &p1->num, &p1->sex, &p1->age, p1->name);
+	printf("2233%d\n", p1->num);
+	printf("2233%d\n", p1->sex);
+	printf("2233%d\n", p1->age);
+	printf("2233%d\n", p1->name);
 	head=NULL;
-	while(p1->num!=0){
-		if(head==NULL)
-			head=p1;
-		else
-			p2->next=p1;
-		p2=p1;
-		p1=(struct student *)malloc(LEN);
-		scanf("%ld,%c,%d,%s",&p1->num,&p1->sex,&p1->age,p1->name);
-	}
+	// while(p1->num!=0){
+	// 	if(head==NULL)
+	// 		head=p1;
+	// 	else
+	// 		p2->next=p1;
+	// 	p2=p1;
+	// 	p1=(struct student *)malloc(LEN);
+	// 	scanf("%ld,%c,%d,%s", &p1->num, &p1->sex, &p1->age, p1->name);
+	// }
 	free(p1);
 	p2->next=NULL;
 	return(head);
@@ -34,8 +40,10 @@ struct student *creat(void){
 void main(){
 	struct student *pt;
  	pt=creat();
-	while(pt!=NULL){
+	while(pt != NULL){
 		printf("\nnum:%ld\tname:%s\tsex:%c\tage:%d",pt->num,pt->name,pt->sex,pt->age);
-		pt=pt->next;
+		pt = pt->next;
 	}
+
+	system("pause");
 }
