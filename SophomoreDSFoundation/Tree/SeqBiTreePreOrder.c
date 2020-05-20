84fcaf2f4ccb9d4c7b4e77dade58d83bb0928c8c
MysticalGuest <1317148109@qq.com>
2020-05-20 09:16:27 +0800

    Create SeqBiTreePreOrder.c

---
+++ SophomoreDSFoundation/Tree/SeqBiTreePreOrder.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define BiTSize 50
+
+int i=0;
+
+typedef struct{
+	char elem[BiTSize];
+}SeqBiTree;
+
+void CreateSeqBiTree(SeqBiTree *bt){
+	char ch;
+	ch=getchar();
+	while(ch!='#'){
+		bt->elem[i]=ch;
+		i++;
+		ch=getchar();
+		if(i==BiTSize-1){
+			printf("Space is Full!");
+			break;
+		}
+	}
+}
+
+void PreOrder(SeqBiTree bt,int j){
+	if(bt.elem[j]!=' '&&j<i){
+		printf("%c",bt.elem[j]);
+		PreOrder(bt,2*j+1);
+		PreOrder(bt,2*j+2);
+	}
+}
+
+void ScanfSeqBiTree(SeqBiTree *bt){
+	printf("Please input some characters(Thereinto 'space' is on behalf of Blank Child) to \nbuild a BiTree(Ending with '#'):\n");
+	CreateSeqBiTree(bt);
+}
+
+void main(){
+	SeqBiTree BT;
+	ScanfSeqBiTree(&BT);
+	printf("The preorder is: ");
+	PreOrder(BT,0);
+	printf("\n");
+}
