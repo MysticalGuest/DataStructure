079ce067244a517b34ea81c60c2bd91802664f5d
MysticalGuest <1317148109@qq.com>
2020-05-22 16:06:19 +0800

    Update task1.c

--- AdvancedJuniorTraining/practice2/task1.c
+++ AdvancedJuniorTraining/practice2/task1.c
@@ -1,31 +1,52 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include<Windows.h>
-// #include<stdlib.h> //itoa所需要的库函数
-
-// 计算最大的n位十进制数
-int maxDecimal(int n){
-  int i=0, max=9, result=0;
+ 
+void print(char *number, int n){
+  int i = 0;
   for(i; i<n; i++){
-    result = result*10 + max;
+    if (number[i] != '0')
+      break;
   }
-  return result;
+  if(i == n)
+    return;
+  for(i; i<n; i++)
+    printf("%c", number[i]);
+  printf(" ");
 }
-
+ 
+void printRecursively(char *number, int n, int index){
+  int i;
+	if(index == n){
+		print(number, n);
+		return;
+	}
+	for(i = 0; i<10; i++){
+		number[index] = i + '0';
+		printf("NUMBER: %s\n", number);
+		printRecursively(number, n, index + 1);
+	}
+}
+ 
+void printToMaxOfNDigits(int n){
+	if (n <= 0)
+		return;
+	char *number = (char *)malloc((n+1)*sizeof(char));	
+  memset(number, '0', (n+1)*sizeof(char));    // 在一段内存块中填充某个给定的值,初始化为0   
+	number[n] = '\0';
+	printRecursively(number, n, 0);
+  
+  free(number);
+}
+ 
 void main(){
+	int n;
   // 输入数字n,按顺序打印输出从1到最大的n位十进制数
   printf("Enter the number n and print out the decimal digits from 1 to the largest n in order: ");
-  int n;
-  long int i=0, count=1;
-  scanf("%d", &n);
-  for(i; i<maxDecimal(n); i++){
-    printf("%d\t", count);
-    // 每10个数换行
-    if(count%10==0){
-      printf("\n");
-    }
-    count++;
-  }
+	scanf("%d", &n);
+	printToMaxOfNDigits(n);
 
-  system("pause");
+  printf("\n");
+	system("pause");
 }
-
