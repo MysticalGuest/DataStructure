ee4a49392df98d26507aa27220b51375912918ed
MysticalGuest <1317148109@qq.com>
2020-05-22 23:34:02 +0800

    Update task2.c

--- AdvancedJuniorTraining/practice3/task2.c
+++ AdvancedJuniorTraining/practice3/task2.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
 #include<Windows.h>
 
+void sort(int a[], int length){
+  int i, j, min, temp;
+  for(i=0; i<length; i++){
+    min=i;
+    for(j=i; j<length; j++){
+      if(a[min]>a[j])
+        min=j;
+    }
+    if(min!=i){
+      temp=a[min];
+      a[min]=a[i];
+      a[i]=temp;
+    }
+  }
+}
+
 void main(){
+  int array[9] = {1,2,3,2,2,2,5,4,2}, i, k;
+  printf("Give an array: \n");
+  for(i=0; i<9; i++){
+    printf("%d\t", array[i]);
+  }
+  sort(array, 8);
+  printf("\nElements that occur more than half the length of the array are :");
+  
+  printf("%d\n", array[9/2]);
   system("pause");
 }
