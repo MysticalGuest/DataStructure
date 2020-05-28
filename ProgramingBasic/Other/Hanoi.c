36f112c91fa1e49f1e274db2f46d272489c3a39f
MysticalGuest <1317148109@qq.com>
2020-05-28 22:15:14 +0800

    Update Hanoi.c

--- ProgramingBasic/Other/Hanoi.c
+++ ProgramingBasic/Other/Hanoi.c
@@ -10,12 +10,25 @@ void hanoi(int n,char x,char y,char z){
 	else{
 	}
 }
+
+void move1(int n,char A,char B,char C){
+  if(n==1){
+    printf("%c-->%c\n",A,C);	
+  }
+  else{
+    move1(n-1,A,C,B);
+    move1(1,A,B,C);
+    move1(n-1,B,A,C);
+  }	
+}
+
 void main(){
 	int n;
-	char A,B,C;
-	printf("This is a Hanoi Problme.Please input how many plates you want: ");
+	printf("This is a Hanoi Problme.\nPlease input how many plates you want: ");
 	scanf("%d",&n);
-	hanoi(n,'A','B','C');
+	// hanoi(n,'A','B','C');
+
+	move1(n, 'A', 'B', 'C');
 
 	system("pause");
 }
