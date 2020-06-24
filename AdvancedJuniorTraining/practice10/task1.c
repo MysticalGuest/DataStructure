3a247ea682f963d0ccd669878e6c8235cd4b5316
MysticalGuest <1317148109@qq.com>
2020-06-24 11:46:04 +0800

    Update task1.c

--- AdvancedJuniorTraining/practice10/task1.c
+++ AdvancedJuniorTraining/practice10/task1.c
@@ -2,32 +2,32 @@
 #include<Windows.h>
 
 typedef struct Node{
-	char elem;
-	struct Node *LChild;
-	struct Node *RChild;
+  char elem;
+  struct Node *LChild;
+  struct Node *RChild;
 }BiTNode,*BiTree;
 
 void CreateBiTree(BiTree *bt, char *list, int locate){
   char ch = list[locate];
-	if(ch=='#')
-		(*bt)=NULL;
-	else{
-		(*bt)=(BiTree)malloc(sizeof(BiTNode));
-		(*bt)->elem=ch;
-		CreateBiTree(&((*bt)->LChild), list, 2*locate+1);
-		CreateBiTree(&((*bt)->RChild), list, 2*locate+2);
-	}
+  if(ch=='#')
+    (*bt)=NULL;
+  else{
+    (*bt)=(BiTree)malloc(sizeof(BiTNode));
+    (*bt)->elem=ch;
+    CreateBiTree(&((*bt)->LChild), list, 2*locate+1);
+    CreateBiTree(&((*bt)->RChild), list, 2*locate+2);
+  }
 }
 
 void PrintTree(BiTree bt,int nLayer){
-	int i;
-	if(bt==NULL)
-		return;
-	PrintTree(bt->RChild,nLayer+1);
-	for(i=0;i<nLayer;i++)
-		printf(" ");
-	printf("%c\n", bt->elem);
-	PrintTree(bt->LChild,nLayer+1);
+  int i;
+  if(bt==NULL)
+    return;
+  PrintTree(bt->RChild,nLayer+1);
+  for(i=0;i<nLayer;i++)
+    printf(" ");
+  printf("%c\n", bt->elem);
+  PrintTree(bt->LChild,nLayer+1);
 }
 
 // 1代表是正则二叉树，0代表不是
