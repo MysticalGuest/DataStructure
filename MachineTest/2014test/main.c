f2b86b7442ad187aaac61f31899201949c21be19
MysticalGuest <1317148109@qq.com>
2021-03-03 10:00:00 +0800

    Update main.c

--- MachineTest/2014test/main.c
+++ MachineTest/2014test/main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int simple_calculation();
+int prime();
+int sieve();
+int string_statistics();
+
 int main()
 {
     //printf("Hello world!\n");
     //wage();
     //monkey_peanuts();
-    ski();
+    //ski();
+    //simple_calculation();
+    //prime();
+    //sieve();
+    string_statistics();
     return 0;
 }
