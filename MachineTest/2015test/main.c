3cb9f8bbaafc9918772ebf192de073cc70582d26
MysticalGuest <1317148109@qq.com>
2021-03-03 18:49:08 +0800

    Create main.c

---
+++ MachineTest/2015test/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mininum();
+int decimal_to_binary();
+int maze();
+int another_maze();
+int string_replace();
+
+int main()
+{
+    //printf("Hello world!\n");
+    //mininum();
+    //decimal_to_binary();
+    //maze();
+    //another_maze();
+    string_replace();
+    return 0;
+}
