8c2708668fac46564c93f497df6d0d2ffc29c7a9
MysticalGuest <1317148109@qq.com>
2021-03-10 20:24:11 +0800

    Create main.cpp

---
+++ MachineTest/2018test/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int multiply();
+int factorial_main();
+int arrange();
+int study1();
+int study2();
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    //multiply();
+    //factorial_main();
+    //arrange();
+    //study1();
+    study2();
+    return 0;
+}
