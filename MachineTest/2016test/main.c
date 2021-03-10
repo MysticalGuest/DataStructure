be7ca8c21f60457f4516b3f21e68058776ad8514
MysticalGuest <1317148109@qq.com>
2021-03-10 20:23:51 +0800

    Update main.c

--- MachineTest/2016test/main.c
+++ MachineTest/2016test/main.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 
 int average_time();
+int multiple_sort();
+int calculator();
+int caculate_area();
 
 int main()
 {
     //printf("Hello world!\n");
-    average_time();
+    //average_time();
+    //multiple_sort();
+    calculator();
+    //caculate_area();
     return 0;
 }
