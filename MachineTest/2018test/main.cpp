bc4ea19fc60a4a6cc4f9874315b3a9c1b7d0e613
MysticalGuest <1317148109@qq.com>
2021-03-18 16:22:06 +0800

    Update main.cpp

--- MachineTest/2018test/main.cpp
+++ MachineTest/2018test/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int multiply();
 int factorial_main();
 int arrange();
+int my_sort();
 int study1();
 int study2();
 
@@ -13,7 +14,8 @@ int main()
     //multiply();
     //factorial_main();
     //arrange();
+    my_sort();
     //study1();
-    study2();
+    //study2();
     return 0;
 }
