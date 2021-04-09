7ba9f7b5dce8b6b4659e66fa9e6f62e5d73b0bb1
MysticalGuest <1317148109@qq.com>
2021-04-09 11:43:47 +0800

    Create main.c

---
+++ MachineTest/2021test/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void yanghui1();
+void yanghui2();
+int file_op1();
+int file_op2();
+
+int main()
+{
+    //printf("Hello world!\n");
+    //yanghui1();
+    //yanghui2();
+    //file_op1();
+    file_op2();
+    return 0;
+}
